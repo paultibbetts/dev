17a877e8abad7ed03e4a7255593af5eb680fe927
Paul Tibbetts <code@paultibbetts.uk>
2025-05-24 23:28:16 +0100

    feat: search all files in nvim

--- .config/nvim/lua/custom/plugins/keymaps.lua
+++ .config/nvim/lua/custom/plugins/keymaps.lua
@@ -1,3 +1,9 @@
+local telescope = require 'telescope.builtin'
 return {
   vim.keymap.set('n', '<leader>sp', '<cmd>silent !tmux neww -n tmux-sessionizer tmux-sessionizer<CR>'),
+  vim.keymap.set('n', '<leader>sF', function()
+    telescope.find_files {
+      find_command = { 'rg', '--ignore', '--hidden', '--files' },
+    }
+  end, { desc = '[S]earch all [F]iles even hidden ones' }),
 }
