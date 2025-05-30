30e3106439b77b25d73b20ef80139f2cc1ac685f
Paul Tibbetts <code@paultibbetts.uk>
2025-05-30 18:09:38 +0100

    feat: fzf ctrl+t

--- zsh/fzf.sh
+++ zsh/fzf.sh
@@ -1,2 +1,9 @@
 eval "$(fzf --zsh)"
 
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
+
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
