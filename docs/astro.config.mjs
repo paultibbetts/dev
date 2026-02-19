d3c9b37c43e775aaff8daa41c074bb641eaa2fd8
Paul Tibbetts <code@paultibbetts.uk>
2026-02-19 18:28:03 +0000

    style: apply editorconfig changes to astro config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,60 +6,62 @@ const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
-	site: isProd ? "https://dev.paultibbetts.uk" : undefined,
-	base: "/",
-	integrations: [
-		starlight({
-			title: "dev.paultibbetts.uk",
-			social: [{
-				icon: "github",
-				label: "GitHub",
-				href: "https://github.com/paultibbetts/dev",
-			}],
-			sidebar: [
-				{
-					label: "README",
-					slug: "readme",
-				},
-				{
-					label: "./env",
-					items: [
-						{
-							label: "config",
-							slug: "env/config",
-						},
-						{
-							label: "local",
-							slug: "env/local",
-						},
-						{
-							label: "zsh",
-							slug: "env/zsh",
-						},
-					],
-				},
-				{
-					label: "./run",
-					items: [
-						{
-							label: "Bootstrap",
-							slug: "runs/bootstrap",
-						},
-						{
-							label: "System",
-							slug: "runs/system",
-						},
-						{
-							label: "Runtimes",
-							slug: "runs/runtimes",
-						},
-						{
-							label: "Tools",
-							slug: "runs/tools",
-						},
-					],
-				},
-			],
-		}),
-	],
+  site: isProd ? "https://dev.paultibbetts.uk" : undefined,
+  base: "/",
+  integrations: [
+    starlight({
+      title: "dev.paultibbetts.uk",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/paultibbetts/dev",
+        },
+      ],
+      sidebar: [
+        {
+          label: "README",
+          slug: "readme",
+        },
+        {
+          label: "./env",
+          items: [
+            {
+              label: "config",
+              slug: "env/config",
+            },
+            {
+              label: "local",
+              slug: "env/local",
+            },
+            {
+              label: "zsh",
+              slug: "env/zsh",
+            },
+          ],
+        },
+        {
+          label: "./run",
+          items: [
+            {
+              label: "Bootstrap",
+              slug: "runs/bootstrap",
+            },
+            {
+              label: "System",
+              slug: "runs/system",
+            },
+            {
+              label: "Runtimes",
+              slug: "runs/runtimes",
+            },
+            {
+              label: "Tools",
+              slug: "runs/tools",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
